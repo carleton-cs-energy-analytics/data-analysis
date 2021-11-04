37d8224f5361bcd8f4b288fec21ff78365a19a43
Dominic Enriquez <39425629+enriquezd@users.noreply.github.com>
2021-11-04 11:17:12 -0500

    divided k = 1 and generalizable k into two files while debugging the latter

--- 1means.py
+++ 1means.py
@@ -1,8 +1,8 @@
 """
-kmeans.py
+1means.py
 
 A python implementation of the implementation and the experimentation of the K-Means clustering algorithm as a means of detecting
-anamolies in Carleton's energy data.
+anamolies in Carleton's energy data. Currently only works for k = 1.
 
 Articles referenced:
 https://www.datatechnotes.com/2020/05/anomaly-detection-with-kmeans-in-python.html
@@ -30,53 +30,29 @@ from scipy.spatial.distance import cdist
 from psycopg2 import connect, sql
 from datetime import datetime
 
-def get_distances_and_points(data, centers, labels):
-    '''
-    '''
-    distances_by_cluster = []
-    indices_by_cluster = []
-    for i, center in enumerate(centers):
-        cluster = np.where(labels == i)[0]
-        distance = np.sqrt((cluster - center) ** 2)
-        distances_by_cluster.append(distance)
-        indices_by_cluster.append(cluster)
-    return distances_by_cluster, indices_by_cluster
-
-def get_outliers(distances, indices, percentile):
-    '''
-    '''
-    outliers = []
-    for i in range(len(distances)):
-        outlier_indices = indices[i][np.where(distances[i] > np.percentile(distances[i], percentile))]
-        outliers.append(outlier_indices)
-    return np.array(outliers).flatten()
-
-def k_means(data, k = 3, plot = True, percentile = 90):
-    '''
-    '''
+def k_means(data, n = 5, plot = True):
     x_axis = range(data.shape[1])
     room_temp = data[1,:]
     room_temp = room_temp.reshape(-1, 1)
 
-    kmeans = KMeans(n_clusters = k).fit(room_temp)
-    
+    kmeans = KMeans(n_clusters = 1).fit(room_temp)
+
     centers = kmeans.cluster_centers_
-    labels = kmeans.labels_
 
-    distances, indices = get_distances_and_points(room_temp, centers, labels)
-    outliers = get_outliers(distances, indices, percentile)
+    distance = np.sqrt((room_temp - centers)**2)
+    ordered_indecies = np.argsort(distance, axis = 0)
+    indecies = ordered_indecies[-n:]
+    values = room_temp[indecies]
 
-    values = room_temp[outliers]
-    
     if plot:
         plt.plot(x_axis, room_temp)
-        plt.scatter(outliers, values, color = 'r')
+        plt.scatter(indecies, values, color = 'r')
         plt.show()
     return kmeans
 
 def main():
     points_array = np.genfromtxt('evans_points.csv', delimiter=',')
-    k_means(points_array, k = 3)
+    kmeans = k_means(points_array)
 
 if __name__ == '__main__':
     main()
