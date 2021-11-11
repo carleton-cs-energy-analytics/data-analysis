4a6b9aedd4f5cb56a81d0cea085073e990b263f8
Dominic Enriquez <39425629+enriquezd@users.noreply.github.com>
2021-11-11 12:53:42 -0600

    stylistic changes to kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -8,9 +8,9 @@ Articles referenced:
 https://www.datatechnotes.com/2020/05/anomaly-detection-with-kmeans-in-python.html
 https://medium.datadriveninvestor.com/outlier-detection-with-k-means-clustering-in-python-ee3ac1826fb0
 
-Written by Dominic Enriquez in collaboration with Ben Preiss
+Written by Dominic Enriquez and Ben Preiss
 2 November 2021
-Last Modified: 4 November 2021
+Last Modified: 11 November 2021
 """
 
 import os
@@ -37,51 +37,43 @@ def generate_test_data():
     # for each hypothetical room that we want to track
     for _ in range(100):
         room = []
-        # generate 96 temperature readings associated with a particular time index
-        for i in range(15):
-            a = np.random.normal(65, 5)
-            room.append(a)
+        # generate 15 temperature readings associated with a particular time index
+        for _ in range(15):
+            room.append(np.random.normal(65, 5))
         data.append(room)
     return np.array(data)
 
-def k_means(data, k = 3, plot = True):
+def k_means(data, k = 3):
     '''
     '''
     km = KMeans(n_clusters = k)
     distance_from_all_clusters = km.fit_transform(data)
     min_distances = [min(distances) for distances in distance_from_all_clusters]
-    sum_distances = [sum(distances) for distances in distance_from_all_clusters]
+    #sum_distances = [sum(distances) for distances in distance_from_all_clusters]
 
     mean = np.mean(min_distances)
     sd = np.std(min_distances)
 
-    indices = []
+    anomalous_rooms = []
     for room_index, distance in enumerate(min_distances):
         if (distance > (mean + 2.3 * sd)):
             print("Room %d is anomalous" % room_index)
-            indices.append(room_index)
-    for index, temperature_data in enumerate(data):
-        if index in indices:
-            #make it red
-            times = []
-            temps = []
-            for time, temp in enumerate(temperature_data):
-                times.append(time)
-                temps.append(temp)
+            anomalous_rooms.append(room_index)
+    for room_index, temperature_data in enumerate(data):
+        times = []
+        temps = []
+        for time, temp in enumerate(temperature_data):
+            times.append(time)
+            temps.append(temp)
+        if room_index in anomalous_rooms:
+            #make anamolies red
             plt.plot(times, temps, c = 'r', zorder = 2)
         else:
-            times = []
-            temps = []
-            for time, temp in enumerate(temperature_data):
-                times.append(time)
-                temps.append(temp)
             plt.plot(times, temps, c = 'k', zorder = 1)
     plt.ylim(30,100)
-    if (len(indices) == 0):
+    if (len(anomalous_rooms) == 0):
         print("No anomalies detected")
     plt.show()
-    if plot:
-        pass
 
 def main():
     data = generate_test_data()
